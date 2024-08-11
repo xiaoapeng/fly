05c81ff0f2f2c1c88b4fed7b2cabd2917eeee55d
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-08-11 14:21:02 +0800

    添加 st32f0xx 支持

---
+++ arch/st32f0xx/gcc-config.cmake
@@ -0,0 +1,21 @@
+CMAKE_MINIMUM_REQUIRED(VERSION 3.10)
+SET(DEMAND_CMAKE_SYSTEM_PROCESSOR cortex-m0)
+
+set(DEMAND_COMPILER_OPTIONS
+    "-mcpu=cortex-m0"
+    "-ffunction-sections"
+    "-fdata-sections"
+    "-mfloat-abi=softfp"
+)
+
+set(DEMAND_LINK_OPTIONS
+    "-mcpu=cortex-m0"
+    "-mfloat-abi=softfp"
+    "-ffunction-sections" 
+    "-fdata-sections" 
+    "-specs=nano.specs" 
+    "-specs=nosys.specs" 
+    "-Wl,--gc-sections"
+)
+
+set(DEMAND_COMPILE_TOOL_CHAIN "gcc-arm-none-eabi")
