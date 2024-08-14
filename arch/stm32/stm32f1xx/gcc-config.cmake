03aab3854453f28df571704284619038cdaf5889
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-08-14 22:13:31 +0800

    添加stm32f103c6支持

---
+++ arch/stm32/stm32f1xx/gcc-config.cmake
@@ -0,0 +1,21 @@
+CMAKE_MINIMUM_REQUIRED(VERSION 3.10)
+SET(DEMAND_CMAKE_SYSTEM_PROCESSOR cortex-m3)
+
+set(DEMAND_COMPILER_OPTIONS
+    "-mcpu=cortex-m3"
+    "-ffunction-sections"
+    "-fdata-sections"
+    "-mfloat-abi=softfp"
+)
+
+set(DEMAND_LINK_OPTIONS
+    "-mcpu=cortex-m3"
+    "-mfloat-abi=softfp"
+    "-ffunction-sections" 
+    "-fdata-sections"
+    "-specs=nano.specs" 
+    "-specs=nosys.specs" 
+    "-Wl,--gc-sections"
+)
+
+set(DEMAND_COMPILE_TOOL_CHAIN "gcc-arm-none-eabi")
---
+++ project/stm32f1xx/stm32f103c6-demo/app/init.c
@@ -0,0 +1,70 @@
+/**
+ * @file init.c
+ * @brief 
+ * @author simon.xiaoapeng (simon.xiaoapeng@gmail.com)
+ * @version 1.0
+ * @date 2024-08-10
+ * 
+ * @copyright Copyright (c) 2024  simon.xiaoapeng@gmail.com
+ * 
+ * @par 修改日志:
+ */
+
+#include "stm32f1xx_ll_bus.h"
+#include "stm32f1xx_ll_rcc.h"
+#include "stm32f1xx_ll_system.h"
+#include "stm32f1xx_ll_utils.h"
+
+
+void SystemClock_Config(void)
+{
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
+  while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_2)
+  {
+  }
+  LL_RCC_HSE_Enable();
+
+   /* Wait till HSE is ready */
+  while(LL_RCC_HSE_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE_DIV_1, LL_RCC_PLL_MUL_9);
+  LL_RCC_PLL_Enable();
+
+   /* Wait till PLL is ready */
+  while(LL_RCC_PLL_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+
+  }
+  LL_Init1msTick(72000000);
+  LL_SetSystemCoreClock(72000000);
+}
+
+
+
+
+int init(void){
+    
+    LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_AFIO);
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+
+    LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOC);
+
+    /* SysTick_IRQn interrupt configuration */
+    NVIC_SetPriority(SysTick_IRQn, 3);
+    SystemClock_Config();
+    
+
+    return 0;
+}
