d6b551ada0eaf5928514046f83bee56d74666f69
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-08-16 22:32:51 +0800

    添加stm32f401xc支持

---
+++ project/stm32f4xx/stm32f401cb-demo/board/led.c
@@ -0,0 +1,41 @@
+/**
+ * @file led.c
+ * @brief led控制驱动
+ * @author simon.xiaoapeng (simon.xiaoapeng@gmail.com)
+ * @version 1.0
+ * @date 2024-07-20
+ * 
+ * @copyright Copyright (c) 2024  simon.xiaoapeng@gmail.com
+ * 
+ * @par 修改日志:
+ */
+
+#include "eh.h"
+#include "eh_module.h"
+
+#include "led.h"
+
+#include "stm32f4xx_ll_gpio.h"
+#include "stm32f4xx_ll_bus.h"
+
+
+int __init board_led_init(void)
+{
+    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    /* GPIO Ports Clock Enable */
+    /**/
+    LL_GPIO_ResetOutputPin(RUN_LED_GPIO_GPIO_Port, RUN_LED_GPIO_Pin);
+
+    /**/
+    GPIO_InitStruct.Pin = RUN_LED_GPIO_Pin;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    LL_GPIO_Init(RUN_LED_GPIO_GPIO_Port, &GPIO_InitStruct);
+    return 0;
+}
+
+
+
+eh_module_export(board_led_init, NULL);
