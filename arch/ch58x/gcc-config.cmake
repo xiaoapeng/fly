8c8eea02030a20a6a9a8d73c73fc7836a18b106e
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-08-27 23:22:37 +0800

    feat(ch58x): 新增ch58x架构支持
    添加了对ch58x架构的支持，包括Kconfig设置、CMake构建和RISC-V核心外设访问层。并添加基础demo，保证编译成功。

---
+++ arch/ch58x/gcc-config.cmake
@@ -0,0 +1,27 @@
+CMAKE_MINIMUM_REQUIRED(VERSION 3.5)
+SET(DEMAND_CMAKE_SYSTEM_PROCESSOR riscv)
+
+set(DEMAND_COMPILER_OPTIONS
+    "-march=rv32imac"
+    "-mabi=ilp32"
+    "-mcmodel=medany"
+    "-msmall-data-limit=8"
+    "-fmessage-length=0"
+    "-fsigned-char"
+    "-fno-common"
+    "-ffunction-sections"
+    "-fdata-sections"
+)
+
+set(DEMAND_LINK_OPTIONS
+    "-march=rv32imac"
+    "-mabi=ilp32"
+    "--specs=nano.specs"
+    "--specs=nosys.specs"
+    "-nostartfiles"
+    "-Xlinker"
+    "--gc-sections"
+)
+
+set(DEMAND_COMPILE_DEFINITIONS "")
+set(DEMAND_COMPILE_TOOL_CHAIN "riscv-none-embed")
