1465a1e30d648dbeb3e3abb229defc8265a5d7ec
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-08-31 23:26:32 +0800

    refactor(cmake): 简化JLink集成并改进VSCode调试配置
    - 从CMakeLists.txt中移除旧的JLink和JLink GDB配置。
    - 添加新的JLink_make_image和VSCode Cortex Debug GDB配置。
    - 在stm32f401cb-demo和stm32h750vb-demo项目的CMakeLists.txt中更新JLink镜像生成和VSCode调试配置。
    - 删除不再使用的mk_vscode_jlink_gnu_gdb.py脚本。

---
+++ tool/python/mk_vscode_cortex_debug_gdb.py
@@ -0,0 +1,112 @@
+import argparse
+import os
+import platform
+import json
+
+def launch_json_read_or_init(top_path, config_name):
+    # launch_json_path = f'{top_path}/.vscode/launch.json'
+    launch_json = {
+        "version": "0.2.0",
+        "configurations": [
+        ]
+    }
+    # if not os.path.exists(launch_json_path):
+    #     return launch_json
+
+    # try:
+    #     with open(launch_json_path, 'r') as file:
+    #         launch_json = json.load(file)
+    # except:
+    #     return launch_json
+    
+    # for config in launch_json['configurations']:
+    #     if config['name'] == config_name:
+    #         # 删除这一项
+    #         launch_json['configurations'].remove(config)
+
+    return launch_json
+
+def launch_json_append(launch_json, top_path, config_name, chip_name, gdb_path, objdump_path, 
+    elf_path_list, server_type, config_file_list, rtt_enabled):
+
+    for elf_path in elf_path_list:
+        file_name = os.path.basename(elf_path)
+        launch_json['configurations'].append({
+            "name": f'{config_name}-{file_name} (attach)',
+            "cwd": "${workspaceFolder}",
+            "type": "cortex-debug",
+            "executable": elf_path,
+            "request": "attach",
+            "servertype": server_type,
+            "device": chip_name,
+            "objdumpPath": objdump_path,
+            "gdbPath": gdb_path,
+            "rttConfig":{
+                "enabled": rtt_enabled,
+                "address": "auto",
+                "decoders": [
+                    {
+                        "label": "RTT OUT",
+                        "port": 0,
+                        "type": "console"
+                    }
+                ]
+            },
+            "configFiles": config_file_list
+        })
+        launch_json['configurations'].append({
+            "name": f'{config_name}-{file_name}',
+            "cwd": "${workspaceFolder}",
+            "type": "cortex-debug",
+            "executable": elf_path,
+            "request": "launch",
+            "breakAfterReset":True,
+            "servertype": server_type,
+            "device": chip_name,
+            "objdumpPath": objdump_path,
+            "gdbPath": gdb_path,
+            "rttConfig":{
+                "enabled": rtt_enabled,
+                "address": "auto",
+                "decoders": [
+                    {
+                        "label": "RTT OUT",
+                        "port": 0,
+                        "type": "console"
+                    }
+                ]
+            },
+            "configFiles": config_file_list
+        })
+
+def launch_json_save(launch_json, top_path):
+    launch_json_path = f'{top_path}/.vscode/launch.json'
+    with open(launch_json_path, 'w') as file:
+        json.dump(launch_json, file, indent=4)
+
+
+
+if __name__ == '__main__':
+    # ['/home/simon/project/fly/tool/python/mk_vscode_jlink_gnu_gdb.py', '--chip-name', 'STM32H750VB', '--gdb-path', '/usr/bin/gdb-multiarch', '--elf-path', '/home/simon/project/fly/build/project/stm32h7xx/stm32h750vb-demo/stm32h750vb_demo_app', '--speed', '10000']
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server-type', type=str, required=True, help='GDB Server type - supported types are jlink, openocd, pyocd, pe, stlink, stutil, qemu, bmp and external. For \"external\", [please read our Wiki](https://github.com/Marus/cortex-debug/wiki/External-gdb-server-configuration). The executable in your PATH is used by default, to override this use serverpath.')
+    parser.add_argument('--config-name', type=str, required=True, help='config name')
+    parser.add_argument('--top-path', type=str, required=True, help='top path')
+    parser.add_argument('--chip-name', type=str, required=True, help='chip name')
+    parser.add_argument('--gdb-path', type=str, required=True, help='gdb path')
+    parser.add_argument('--objdump-path', type=str, required=True, help='objdump path')
+    parser.add_argument('--elf-path-list', nargs='+', type=str, required=True, help='elf path')
+    #是否启用rtt
+    parser.add_argument('--rtt-enabled', action='store_true', default=False, help='Enable RTT logging')
+    parser.add_argument('--config-file-list', nargs='+', type=str, required=False, help='OpenOCD/PE GDB Server configuration file(s) to use when debugging (OpenOCD -f option)')
+    
+    args = parser.parse_args()
+
+    launch_json = launch_json_read_or_init(args.top_path, args.config_name)
+    launch_json_append(launch_json, args.top_path, args.config_name, args.chip_name, 
+                        args.gdb_path, args.objdump_path, args.elf_path_list, args.server_type, args.config_file_list, args.rtt_enabled)
+    launch_json_save(launch_json, args.top_path)
+
+
+
+
