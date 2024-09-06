4fd2e424a8d8ceff3905293e2f5b841c6887328c
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-09-06 22:39:17 +0800

    feat(openocd rtt log):新增openocd rtt日志抓取功能
    - 在 cmake 配置中添加了新选项，可以在生成 openocd 镜像时指定 RTT（实时终端）地址和大小，便于生成log自动抓取脚本。
    - 同时，更新了 mk_openocd_img.py 脚本，以接受并处理 RTT 配置参数。

--- cmake/openocd-make-image.cmake
+++ cmake/openocd-make-image.cmake
@@ -6,7 +6,7 @@ function(add_openocd_image CMAKE_TARGET)
     cmake_parse_arguments(
         CUSTOM_FUNC ""
         "DEPENDS;IMAGE_NAME"
-        "FIRMWARE_LIST;CONFIGFILE_LIST"
+        "FIRMWARE_LIST;CONFIGFILE_LIST;RTT_ADDR_AND_SIZE"
         ""
         ${ARGN}
     )
@@ -15,10 +15,22 @@ function(add_openocd_image CMAKE_TARGET)
         set(CUSTOM_FUNC_CONFIGFILE_LIST " ")
     endif()
 
+    # 获取列表长度
+    if( CUSTOM_FUNC_RTT_ADDR_AND_SIZE )
+        list(APPEND FUNC_RTT_ADDR_AND_SIZE_OPTION "--rtt-setting")
+        list(APPEND FUNC_RTT_ADDR_AND_SIZE_OPTION ${CUSTOM_FUNC_RTT_ADDR_AND_SIZE})
+
+        list(LENGTH CUSTOM_FUNC_RTT_ADDR_AND_SIZE CUSTOM_FUNC_RTT_ADDR_AND_SIZE_LENGTH)
+
+        if ( ${CUSTOM_FUNC_RTT_ADDR_AND_SIZE_LENGTH} LESS 2 )
+            message(FATAL_ERROR "RTT_ADDR_AND_SIZE must be a list of two elements")
+        endif()
+    endif()
+
     add_custom_command(
         OUTPUT ${FLY_TOP_DIR}/image/${CUSTOM_FUNC_IMAGE_NAME}/.${CMAKE_TARGET}_make_img.timestamp
         COMMAND ${Python3_EXECUTABLE} ${FLY_TOP_DIR}/tool/python/mk_openocd_img.py 
-            --firmware-name ${CUSTOM_FUNC_IMAGE_NAME}_${CMAKE_BUILD_TYPE}
+            --firmware-name ${CUSTOM_FUNC_IMAGE_NAME}_${CMAKE_BUILD_TYPE} ${FUNC_RTT_ADDR_AND_SIZE_OPTION}
             --output-dir ${FLY_TOP_DIR}/image/${CUSTOM_FUNC_IMAGE_NAME}/ --config-file-list ${CUSTOM_FUNC_CONFIGFILE_LIST} 
             --symlink-name ${CMAKE_TARGET}_openocd_CURRENT --firmware-parts ${CUSTOM_FUNC_FIRMWARE_LIST} 
         COMMAND ${CMAKE_COMMAND} -E touch ${FLY_TOP_DIR}/image/${CUSTOM_FUNC_IMAGE_NAME}/.${CMAKE_TARGET}_make_img.timestamp
