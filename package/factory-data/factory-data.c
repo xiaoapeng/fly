ce799191150f08095e6ffaa91d09f676aeab6a92
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-09-11 00:00:11 +0800

    refactor(package): 实现包装器以自动化子目录的CMake构建
    - 通过文件遍历和条件判断，重构CMakeLists.txt以自动包含package目录下的所有子目录，无需手动添加每个子目录。此更改通过减少手动编辑和潜在错误来简化项目构建配置。
    
    feat(package/factory-data): 添加出厂数据支持模块
    
    - 引入一个新的可选功能模块，允许在MCU固件中检测和读取尾部附加的工厂数据。该模块提供了一个公共接口，用于计算CRC32校验和检测固件中的工厂数据块，其中包含软件版本、分区名称、固件名称和生成描述等重要信息。

---
+++ package/factory-data/factory-data.c
@@ -0,0 +1,66 @@
+/**
+ * @file factory_data.c
+ * @brief 通用的MCU出厂信息获取，
+    通过脚本，在bin文件尾部增加,然后本程序遍历全部的固件，来找到该信息。
+ * @author simon.xiaoapeng (simon.xiaoapeng@gmail.com)
+ * @version 1.0
+ * @date 2024-06-04
+ * 
+ * @copyright Copyright (c) 2024  simon.xiaoapeng@gmail.com
+ * 
+ * @par 修改日志:
+ */
+
+
+#include <stddef.h>
+#include "factory-data.h"
+
+#define POLY 0xEDB88320U
+
+uint32_t factory_data_crc32(uint32_t crc, const uint8_t *msg, uint32_t len){
+    int k;
+
+    crc = ~crc;
+    while (len--) {
+        crc ^= *msg++;
+        for (k = 0; k < 8; k++)
+            crc = crc & 1 ? (crc >> 1) ^ POLY : crc >> 1;
+    }
+    return ~crc;
+}
+
+const FactoryData* factory_data_detect(const void *part_start_addr, uint32_t part_size){
+    const uint8_t *pos_addr = part_start_addr;
+    const uint8_t *end_addr = pos_addr + part_size;
+    const FactoryData* pos_factory_data;
+    uint32_t ffffffff = 0xffffffff;
+    uint32_t crc32_val;
+    /* 
+     * pos_addr 向上8字节对齐  
+     * end_addr 向下8字节对齐
+     */
+    pos_addr = (const uint8_t*)(((uintptr_t)pos_addr+7U) & ~7U);
+    end_addr = (const uint8_t*)((uintptr_t)end_addr & ~7U);
+
+    /* FactoryData 数据必然在8字节对齐的地址处 */
+    for(; end_addr - pos_addr >= (int)sizeof(FactoryData); pos_addr+=8 ){
+        pos_factory_data = (const FactoryData*)pos_addr;
+        if( pos_factory_data->Magic_A != FC_MAGIC_A || 
+            pos_factory_data->Magic_B != FC_MAGIC_B ||
+            pos_factory_data->factory_len < offsetof(FactoryData, software_version) ||
+            pos_factory_data->factory_len > (uint32_t)(end_addr - pos_addr)
+        )
+            continue;
+        crc32_val = ~(0xFFFFFFFF);
+        crc32_val = factory_data_crc32(crc32_val, pos_addr-pos_factory_data->software_len, 
+            pos_factory_data->software_len + offsetof(FactoryData, crc32));
+        crc32_val = factory_data_crc32(crc32_val, (uint8_t*)&ffffffff, sizeof(uint32_t));    /* 中间的crc32值使用0xffffffff代替 */
+        crc32_val = factory_data_crc32(crc32_val, (uint8_t*)(&pos_factory_data->software_version), 
+            pos_factory_data->factory_len - offsetof(FactoryData, software_version));
+        
+        if(crc32_val != pos_factory_data->crc32)
+            continue;
+        return pos_factory_data;
+    }
+    return NULL;
+}
