f3dea0bf2587f7cac6f2413d77c5a12eb8f34c66
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-09-12 20:58:51 +0800

    feat(arch):架构添加gd32vf103x配置支持
    - 在Kconfig中添加gd32vf103x架构的配置选项，并为gd32vf103x创建新的CMake构建文件。
    - 此外，调整现有ch58x配置的编译器设置，避免工具链冲突。

---
+++ arch/gd32/risc-v/gd32vf103/RISCV/drivers/riscv_const.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2019 Nuclei Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+/* Derived from <linux/const.h> */
+
+#ifndef _RISCV_CONST_H
+#define _RISCV_CONST_H
+
+#ifdef __ASSEMBLER__
+#define _AC(X,Y)        X
+#define _AT(T,X)        X
+#else
+#define _AC(X,Y)        (X##Y)
+#define _AT(T,X)        ((T)(X))
+#endif /* !__ASSEMBLER__*/
+
+#define _BITUL(x)       (_AC(1,UL) << (x))
+#define _BITULL(x)      (_AC(1,ULL) << (x))
+
+#endif /* _NUCLEI_CONST_H */
---
+++ cmake/tool_chain/gcc-riscv32-unknown-elf.cmake
@@ -0,0 +1,16 @@
+# the name of the target operating system
+# https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/2024.09.03/riscv32-elf-ubuntu-22.04-gcc-nightly-2024.09.03-nightly.tar.gz
+set(CROSS_COMPILE_PREFIX riscv32-unknown-elf-)
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_VERSION  1)
+set(CMAKE_SYSTEM_PROCESSOR  riscv32)
+
+find_gcc_toolchain(${CROSS_COMPILE_PREFIX})
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# search for program/library/include in the build host directories
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
