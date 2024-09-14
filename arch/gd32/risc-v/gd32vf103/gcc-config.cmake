b9140da24889d7fa90653491914524797b21d4b0
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-09-14 22:19:53 +0800

    perf(arch gcc config):GD32VF103系列的gcc配置(arch/gd32/risc-v/gd32vf103/gcc-config.cmake)优化
    - 在编译器和链接器选项中加入'-msmall-data-limit=8'，优化小数据节的处理。
    - 移除未使用的'-nostdlib'和'-specs=nano.specs'与'-specs=nosys.specs'链接器选项。
    - 删除一些空格。

--- arch/gd32/risc-v/gd32vf103/gcc-config.cmake
+++ arch/gd32/risc-v/gd32vf103/gcc-config.cmake
@@ -2,24 +2,23 @@ CMAKE_MINIMUM_REQUIRED(VERSION 3.5)
 SET(DEMAND_CMAKE_SYSTEM_PROCESSOR riscv32)
 
 set(DEMAND_COMPILER_OPTIONS
-    "-march=rv32imac" 
+    "-march=rv32imac"
     "-mabi=ilp32"
     "-mcmodel=medlow"
     "-ffunction-sections"
     "-fdata-sections"
     "-nostartfiles"
+    "-msmall-data-limit=8"
 )
 
 set(DEMAND_LINK_OPTIONS
-    "-march=rv32imac" 
-    "-mabi=ilp32" 
+    "-march=rv32imac"
+    "-mabi=ilp32"
     "-mcmodel=medlow"
-    "-ffunction-sections" 
-    "-fdata-sections" 
+    "-ffunction-sections"
+    "-fdata-sections"
     "-nostartfiles"
-    # "-nostdlib"
-    "-specs=nano.specs" 
-    "-specs=nosys.specs" 
+    "-msmall-data-limit=8"
     "-Wl,--gc-sections"
 )
 
