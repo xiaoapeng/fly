fdb74d4fe4bae6b6de41bbb05df5b46f1a118751
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-09-21 18:00:03 +0800

    feat(project): 在GD32VF103C演示项目中，新增了LED和按键的驱动支持，并进行了系统时钟的配置。
    - 在`CMakeLists.txt`中调整了编译链接设置，以适应新的硬件抽象层。
    - 在`init.c`中定义了系统时钟并进行了初始化。
    - 在`led.c`和`button.c`中实现了基础的LED控制和按键处理逻辑，包括中断服务例程。
    - 更新了`main.c`，并重构了主程序结构，实现了基本的事件驱动演示。

---
+++ project/gd32vf103x/gd32vf103c-demo/board/button.c
@@ -0,0 +1,77 @@
+/**
+ * @file button.c
+ * @brief 基础按键驱动
+ * @author simon.xiaoapeng (simon.xiaoapeng@gmail.com)
+ * @version 1.0
+ * @date 2024-09-21
+ * 
+ * @copyright Copyright (c) 2024  simon.xiaoapeng@gmail.com
+ * 
+ * @par 修改日志:
+ */
+
+
+#include "eh.h"
+#include "eh_module.h"
+#include "eh_signal.h"
+#include "button.h"
+
+EH_DEFINE_SIGNAL(button_pb5_signal);
+EH_DEFINE_SIGNAL(button_pa0_signal);
+
+
+void EXTI0_IRQHandler(void){
+    if(RESET != exti_interrupt_flag_get(EXTI_0)) {
+        exti_interrupt_flag_clear(EXTI_0);
+        eh_signal_notify(&button_pa0_signal);
+    }
+}
+
+void EXTI5_9_IRQHandler(void){
+    if(RESET != exti_interrupt_flag_get(EXTI_5)) {
+        exti_interrupt_flag_clear(EXTI_5);
+        eh_signal_notify(&button_pb5_signal);
+    }
+}
+
+
+static int __init board_button_init(void)
+{
+    eh_signal_register(&button_pb5_signal);
+    eh_signal_register(&button_pa0_signal);
+    
+    rcu_periph_clock_enable(BUTTON_PB5_GPIO_CLK);
+    gpio_init(BUTTON_PB5_GPIO_PORT, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, BUTTON_PB5_PIN);
+    
+    eclic_irq_enable(BUTTON_PB5_EXTI, 1U, 0U);
+
+    gpio_exti_source_select(GPIO_PORT_SOURCE_GPIOB, GPIO_PIN_SOURCE_5);
+
+    exti_init(EXTI_5, EXTI_INTERRUPT, EXTI_TRIG_FALLING);
+    exti_interrupt_flag_clear(EXTI_5);
+
+    
+    rcu_periph_clock_enable(BUTTON_PA0_GPIO_CLK);
+    gpio_init(BUTTON_PA0_GPIO_PORT, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, BUTTON_PA0_PIN);
+    
+    eclic_irq_enable(BUTTON_PA0_EXTI, 2U, 0U);
+
+    gpio_exti_source_select(GPIO_PORT_SOURCE_GPIOA, GPIO_PIN_SOURCE_0);
+
+    exti_init(EXTI_0, EXTI_INTERRUPT, EXTI_TRIG_FALLING);
+    exti_interrupt_flag_clear(EXTI_0);
+
+
+    return 0;
+}
+
+static void __exit board_button_exit(void){
+    eh_signal_unregister(&button_pa0_signal);
+    eh_signal_unregister(&button_pb5_signal);
+}
+
+eh_module_level0_export(board_button_init, board_button_exit);
+
+
+
+
---
+++ project/gd32vf103x/gd32vf103c-demo/board/init.c
@@ -0,0 +1,42 @@
+/**
+ * @file init.c
+ * @brief 
+ * @author simon.xiaoapeng (simon.xiaoapeng@gmail.com)
+ * @version 1.0
+ * @date 2024-09-16
+ * 
+ * @copyright Copyright (c) 2024  simon.xiaoapeng@gmail.com
+ * 
+ * @par 修改日志:
+ */
+
+#include "SEGGER_RTT.h"
+#include "eh.h"
+#include "eh_debug.h"
+#include "gd32vf103.h"
+#include "n200_func.h"
+#include "n200_timer.h"
+
+
+void stdout_write(void *stream, const uint8_t *buf, size_t size){
+    (void)stream;
+    SEGGER_RTT_Write(0, buf, (unsigned)size);
+}
+
+eh_clock_t  platform_get_clock_monotonic_time(void){
+    return (eh_clock_t)get_timer_value();
+}
+
+void _init();
+
+int init(void){
+    _init();
+    
+    rcu_periph_clock_enable(RCU_AF);
+
+    eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL3_PRIO1);
+
+    eh_infoln("get_cpu_freq: %d", get_cpu_freq());
+    
+    return 0;
+}
