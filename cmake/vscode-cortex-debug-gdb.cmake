56d942e9041af19665dfa44936eef38ecf26628b
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-09-22 17:12:47 +0800

    refactor(cmake, vscode, gd32vf103c-demo): 更新cortex调试配置,支持外部配置，修改部分辨识度低的名称。
    - 此更改支持通过外部JSON文件指定额外的调试配置选项，旨在简化调试会话的设置和管理。
    - 修改CONFIGFILE_LIST为OPENOCD_CONFIGFILE_LIST，提高辨识度。

--- cmake/vscode-cortex-debug-gdb.cmake
+++ cmake/vscode-cortex-debug-gdb.cmake
@@ -4,8 +4,8 @@ include("${CMAKE_CURRENT_LIST_DIR}/git-status.cmake")
 function(add_vscode_cortex_debug_gdb CMAKE_TARGET)
     cmake_parse_arguments(
         CUSTOM_FUNC ""
-        "CHIP_NAME;DEPENDS;SPEED;SERVER_TYPE;RTT_DEBUG;INTERFACE"
-        "CONFIGFILE_LIST;ELF_NAME_LIST"
+        "CHIP_NAME;DEPENDS;SPEED;SERVER_TYPE;RTT_DEBUG;INTERFACE;OTHER_EXT_CONFIG"
+        "OPENOCD_CONFIGFILE_LIST;ELF_NAME_LIST"
         ""
         ${ARGN}
     )
@@ -13,6 +13,20 @@ function(add_vscode_cortex_debug_gdb CMAKE_TARGET)
     # 设置 SPEED 的默认值
     if(${CUSTOM_FUNC_RTT_DEBUG})
         set(RTT_EN_FLAGS "--rtt-enabled")
+    endif()
+    
+    # 追加的config配置项，最后合并到launch.json中去
+    # 基本格式
+    # {
+    #     "ext-attach-config":{
+    #        /* 写这里追加到attach的配置项 */
+    #     },
+    #     "ext-launch-config":{
+    #        /* 写这里追加到launch的配置项 */
+    #     }
+    # }
+    if( CUSTOM_FUNC_OTHER_EXT_CONFIG )
+        set(CUSTOM_FUNC_OTHER_EXT_CONFIG_OPTION "--ext-config-json")
     endif()
 
     if(NOT CUSTOM_FUNC_SPEED)
@@ -28,14 +42,14 @@ function(add_vscode_cortex_debug_gdb CMAKE_TARGET)
         set(CUSTOM_FUNC_INTERFACE "swd")
     endif()
 
-    if(NOT CUSTOM_FUNC_CONFIGFILE_LIST )
-        set(CUSTOM_FUNC_CONFIGFILE_LIST " ")
+    if(NOT CUSTOM_FUNC_OPENOCD_CONFIGFILE_LIST )
+        set(CUSTOM_FUNC_OPENOCD_CONFIGFILE_LIST " ")
     endif()
 
     if(NOT CUSTOM_FUNC_ELF_NAME_LIST)
         message(FATAL_ERROR "No elf file name specified")
     endif()
-
+    
     foreach(ELF_NAME ${CUSTOM_FUNC_ELF_NAME_LIST})
         list(APPEND ELF_PATH_LIST "${CMAKE_CURRENT_BINARY_DIR}/${ELF_NAME}")
         
@@ -54,9 +68,10 @@ function(add_vscode_cortex_debug_gdb CMAKE_TARGET)
     add_custom_command(
         OUTPUT ${CMAKE_BINARY_DIR}/.${CMAKE_TARGET}_make_vscode_jlink_gnu_gdb.timestamp
         COMMAND ${Python3_EXECUTABLE} ${FLY_TOP_DIR}/tool/python/mk_vscode_cortex_debug_gdb.py
-            --config-name ${CMAKE_TARGET} --server-type ${CUSTOM_FUNC_SERVER_TYPE}  --top-path ${FLY_TOP_DIR} --interface ${CUSTOM_FUNC_INTERFACE}
-            --chip-name ${CUSTOM_FUNC_CHIP_NAME} --gdb-path "${CMAKE_GDB}" --objdump-path "${CMAKE_OBJDUMP}" ${RTT_EN_FLAGS}
-            --elf-path ${ELF_PATH_LIST} --config-file ${CUSTOM_FUNC_CONFIGFILE_LIST} 
+            --config-name ${CMAKE_TARGET} --server-type ${CUSTOM_FUNC_SERVER_TYPE}  --top-path ${FLY_TOP_DIR} 
+            --interface ${CUSTOM_FUNC_INTERFACE} --chip-name ${CUSTOM_FUNC_CHIP_NAME} --gdb-path "${CMAKE_GDB}" 
+            --objdump-path "${CMAKE_OBJDUMP}" ${RTT_EN_FLAGS}  ${CUSTOM_FUNC_OTHER_EXT_CONFIG_OPTION} ${CUSTOM_FUNC_OTHER_EXT_CONFIG}
+            --elf-path ${ELF_PATH_LIST} --openocd-config-file-list ${CUSTOM_FUNC_OPENOCD_CONFIGFILE_LIST} 
         DEPENDS ${CUSTOM_FUNC_DEPENDS}
         COMMENT "${CUSTOM_FUNC_ELF_NAME}:Make vscode jlink gnu gdb"
     )
