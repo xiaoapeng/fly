8d21540789309c665ff7da24c137c439e1d24432
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-09-23 23:35:25 +0800

    refactor(cmake):重构`add_target_make_bin`函数以优化目标文件的构建过程。此次更改主要包括：
    - 生成独立的调试文件`${ELF_FILE}.debug`，通过`--only-keep-debug`选项保留调试信息。
    - 从主目标文件中移除调试信息，使用`--strip-debug`命令。
    - 添加GNU调试链接到主目标文件，便于后续调试，采用`--add-gnu-debuglink`标志。
    - 最终生成的十六进制文件`${ELF_FILE}.hex`使用`--strip-debug`和`--strip-unneeded`选项，彻底去除不需要的数据。

--- cmake/gcc-tool-chain-general.cmake
+++ cmake/gcc-tool-chain-general.cmake
@@ -6,8 +6,10 @@ function(add_target_make_bin ELF_FILE)
         COMMAND ${CMAKE_SIZE} --format=berkeley $<TARGET_FILE:${ELF_FILE}>
         COMMAND ${CMAKE_OBJDUMP} --all-headers --demangle --disassemble $<TARGET_FILE:${ELF_FILE}> > ${ELF_FILE}.list
         COMMAND ${CMAKE_OBJDUMP} -d -S -s $<TARGET_FILE:${ELF_FILE}> > ${ELF_FILE}.S
-        COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:${ELF_FILE}> ${ELF_FILE}.hex
-        #删除 bin文件中的调试信息
+        COMMAND ${CMAKE_OBJCOPY} --only-keep-debug $<TARGET_FILE:${ELF_FILE}> ${ELF_FILE}.debug
+        COMMAND ${CMAKE_OBJCOPY} --strip-debug $<TARGET_FILE:${ELF_FILE}> 
+        COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink=${ELF_FILE}.debug $<TARGET_FILE:${ELF_FILE}> 
+        COMMAND ${CMAKE_OBJCOPY} --strip-debug --strip-unneeded -O ihex $<TARGET_FILE:${ELF_FILE}> ${ELF_FILE}.hex
         COMMAND ${CMAKE_OBJCOPY} --strip-debug --strip-unneeded -O binary $<TARGET_FILE:${ELF_FILE}> ${ELF_FILE}.bin
         COMMENT "Building ${ELF_FILE}\nBuilding ${ELF_FILE}.bin"
     )
