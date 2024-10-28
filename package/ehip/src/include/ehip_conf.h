dcbdc3a73d5b93b2473e4612e639e3e87e6dd280
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-10-28 10:52:16 +0800

    feat(ehip): 添加和修改 ehip_conf.h 中的配置参数
    - 新增以太网帧内存块大小、数量等配置参数
    - 添加 POLL BUFFER 句柄最大数量配置
    - 新增核心程序消息邮箱大小、网卡 RX 消息句柄数量等配置
    - 增加内存池基础数据结构对齐配置

--- package/ehip/src/include/ehip_conf.h
+++ package/ehip/src/include/ehip_conf.h
@@ -12,19 +12,54 @@
 #ifndef _EHIP_CONF_H_
 #define _EHIP_CONF_H_
 
+/*
+ * 以太网帧内存块大小
+ */
 #ifndef EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_SIZE
-#define EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_SIZE  1518U
+#define EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_SIZE              1518U
 #endif
 
+/*
+ * 以太网帧内存块数量
+ */
 #ifndef EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_NUM
-#define EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_NUM   6U
+#define EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_NUM               6U
 #endif
 
+/**
+ * 以太网帧内存块对齐大小
+ */
 #ifndef EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_ALIGN
-#define EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_ALIGN 16U
+#define EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_ALIGN             16U
 #endif
 
+/*
+ *  POLL BUFFER 句柄最大数量，要大于 EHIP_NETDEV_TYPE_ETHERNET_POOL_BUFFER_NUM
+ */
+#ifndef EHIP_NETDEV_POLL_BUFFER_MAX_NUM 
+#define EHIP_NETDEV_POLL_BUFFER_MAX_NUM                         16U
+#endif
 
+/**
+ *  核心程序消息邮箱的大小
+ */
+#ifndef EHIP_CORE_MBOX_MSG_SIZE
+#define EHIP_CORE_MBOX_MSG_SIZE                                 16U
+#endif
+
+/**
+ *  网卡RX 消息句柄数量，单次最多结束的数据包数量
+ */
+#ifndef EHIP_CORE_MBOX_NETDEV_MSG_BUFFER_NUM
+#define EHIP_CORE_MBOX_NETDEV_MSG_BUFFER_NUM                    16U
+#endif
+
+/**
+ *  内存池基础数据结构对齐
+ */
+#ifndef EHIP_POOL_BASE_ALIGN
+#define EHIP_POOL_BASE_ALIGN                                    4U
+#endif
 
 
 #endif // _EHIP_CONF_H_
