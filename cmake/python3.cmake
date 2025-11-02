8f3396d41ecaee2af5334a94b694cc346b37cac7
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2025-11-02 19:55:26 +0800

    feat(cmake): 添加对.venv目录的检查，以优先使用该目录中的Python

--- cmake/python3.cmake
+++ cmake/python3.cmake
@@ -1,4 +1,10 @@
 include_guard(GLOBAL)
 
+# 检查.venv目录是否存在，如果存在则设置为Python根目录
+if(EXISTS "${FLY_TOP_DIR}/.venv")
+    message(STATUS "Found .venv directory, will prioritize Python from here")
+    set(Python3_ROOT_DIR "${FLY_TOP_DIR}/.venv")
+endif()
+
 find_package(Python3 COMPONENTS Interpreter REQUIRED)
 message(STATUS "Python3 Interpreter: ${Python3_EXECUTABLE}")
